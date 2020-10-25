4625437e54e36b91621d9448609ba66a9deb35b0
Jeff Folster <jmf432@mail.usask.ca>
2020-10-25 15:21:41 -0600

    updated Python files to spaces convention over tabs

--- src/Names.py
+++ src/Names.py
@@ -1,203 +1,203 @@
 class Name:
-	"""
-	<<interface>> Name
-	"""
+    """
+    <<interface>> Name
+    """
 
-	def get_name(self):
-		"""
+    def get_name(self):
+        """
 
-		:return:
-		"""
-		pass
+        :return:
+        """
+        pass
 
-	def set_name(self, name):
-		"""
+    def set_name(self, name):
+        """
 
-		:param name:
-		:return:
-		"""
-		pass
+        :param name:
+        :return:
+        """
+        pass
 
 
 class GivenName(Name):
-	"""
+    """
 
-	"""
+    """
 
-	def __init__(self):
-		"""
+    def __init__(self):
+        """
 
-		"""
-		self.givenName = ""
+        """
+        self.givenName = ""
 
-	def get_name(self):
-		"""
+    def get_name(self):
+        """
 
-		:return:
-		"""
-		return self.givenName
+        :return:
+        """
+        return self.givenName
 
-	def set_name(self, name):
-		"""
+    def set_name(self, name):
+        """
 
-		:param name:
-		:return:
-		"""
-		self.givenName = name
+        :param name:
+        :return:
+        """
+        self.givenName = name
 
 
 class MiddleNames(Name):
-	"""
+    """
 
-	"""
+    """
 
-	def __init__(self):
-		"""
+    def __init__(self):
+        """
 
-		"""
-		self.middleNames = []
+        """
+        self.middleNames = []
 
-	def get_name(self):
-		"""
+    def get_name(self):
+        """
 
-		:return:
-		"""
-		return self.middleNames
+        :return:
+        """
+        return self.middleNames
 
-	def get_names_list(self):
-		"""
+    def get_names_list(self):
+        """
 
-		:return:
-		"""
-		return self.middleNames
+        :return:
+        """
+        return self.middleNames
 
-	def get_names_to_string(self):
-		"""
+    def get_names_to_string(self):
+        """
 
-		:return:
-		"""
-		return ' '.join(self.middleNames)
+        :return:
+        """
+        return ' '.join(self.middleNames)
 
-	def set_name(self, name_list):
-		"""
+    def set_name(self, name_list):
+        """
 
-		:param name_list: a list of strings
-		:return:
-		"""
-		self.middleNames = name_list
+        :param name_list: a list of strings
+        :return:
+        """
+        self.middleNames = name_list
 
 
 class Surname(Name):
-	"""
+    """
 
-	"""
+    """
 
-	def __init__(self):
-		"""
+    def __init__(self):
+        """
 
-		"""
-		self.surname = ""
+        """
+        self.surname = ""
 
-	def get_name(self):
-		"""
+    def get_name(self):
+        """
 
-		:return:
-		"""
-		return self.surname
+        :return:
+        """
+        return self.surname
 
-	def set_name(self, name):
-		"""
+    def set_name(self, name):
+        """
 
-		:param name:
-		:return:
-		"""
-		self.surname = name
+        :param name:
+        :return:
+        """
+        self.surname = name
 
 
 class PreferredName(GivenName):
-	"""
-	Behaviour is like GivenName so nothing to add at the moment.
-	"""
+    """
+    Behaviour is like GivenName so nothing to add at the moment.
+    """
 
 
 class FullName:
-	"""
-	Contains the full name of a person.
-	"""
+    """
+    Contains the full name of a person.
+    """
 
-	def __init__(self):
-		"""
+    def __init__(self):
+        """
 
-		"""
-		self.given = GivenName()
-		self.middle = MiddleNames()
-		self.surname = Surname()
-		self.preferred = PreferredName()
+        """
+        self.given = GivenName()
+        self.middle = MiddleNames()
+        self.surname = Surname()
+        self.preferred = PreferredName()
 
-	def get_full_name_to_string(self):
-		"""
+    def get_full_name_to_string(self):
+        """
 
-		:return:
-		"""
-		return self.given.get_name() + " " + self.middle.get_names_to_string() \
-			+ " " + self.surname.get_name()
+        :return:
+        """
+        return self.given.get_name() + " " + self.middle.get_names_to_string() \
+               + " " + self.surname.get_name()
 
-	def get_preferred_name(self):
-		"""
+    def get_preferred_name(self):
+        """
 
-		:return:
-		"""
-		return self.preferred.get_name()
+        :return:
+        """
+        return self.preferred.get_name()
 
-	def get_given_name(self):
-		"""
+    def get_given_name(self):
+        """
 
-		:return:
-		"""
-		return self.given.get_name()
+        :return:
+        """
+        return self.given.get_name()
 
-	def get_middle_name_list(self):
-		"""
+    def get_middle_name_list(self):
+        """
 
-		:return:
-		"""
-		return self.middle.get_names_list()
+        :return:
+        """
+        return self.middle.get_names_list()
 
-	def get_middle_names_to_string(self):
-		"""
+    def get_middle_names_to_string(self):
+        """
 
-		:return:
-		"""
-		return self.middle.get_names_to_string()
+        :return:
+        """
+        return self.middle.get_names_to_string()
 
-	def set_middle_names(self, name_list):
-		"""
+    def set_middle_names(self, name_list):
+        """
 
-		:param name_list:
-		:return:
-		"""
-		self.middle.set_name(name_list)
+        :param name_list:
+        :return:
+        """
+        self.middle.set_name(name_list)
 
-	def set_preferred_name(self, name):
-		"""
+    def set_preferred_name(self, name):
+        """
 
-		:param name:
-		:return:
-		"""
-		self.preferred.set_name(name)
+        :param name:
+        :return:
+        """
+        self.preferred.set_name(name)
 
-	def set_surname(self, name):
-		"""
+    def set_surname(self, name):
+        """
 
-		:param name:
-		:return:
-		"""
-		self.surname.set_name(name)
+        :param name:
+        :return:
+        """
+        self.surname.set_name(name)
 
-	def set_given_name(self, name):
-		"""
+    def set_given_name(self, name):
+        """
 
-		:param name:
-		:return:
-		"""
-		self.given.set_name(name)
+        :param name:
+        :return:
+        """
+        self.given.set_name(name)
--- tests/test_proof_of_concept.py
+++ tests/test_proof_of_concept.py
@@ -4,19 +4,19 @@ from Names import *
 
 # stub test
 def test_stub():
-	assert True
+    assert True
 
 
 def test_middle_names_set_names():
-	mn = MiddleNames()
-	mn.set_name(["Gary", "Kaye"])
-	assert ["Gary", "Kaye"] == mn.get_names_list()
+    mn = MiddleNames()
+    mn.set_name(["Gary", "Kaye"])
+    assert ["Gary", "Kaye"] == mn.get_names_list()
 
 
 def test_full_name_getters_setters():
-	fn = FullName()
-	fn.set_given_name("David")
-	fn.set_middle_names(["Lee", "Roy", "Jenkins"])
-	fn.set_surname("Baesmintdwaellor")
-	fn.set_preferred_name("Dave")
-	assert "David Lee Roy Jenkins Baesmintdwaellor" == fn.get_full_name_to_string()
+    fn = FullName()
+    fn.set_given_name("David")
+    fn.set_middle_names(["Lee", "Roy", "Jenkins"])
+    fn.set_surname("Baesmintdwaellor")
+    fn.set_preferred_name("Dave")
+    assert "David Lee Roy Jenkins Baesmintdwaellor" == fn.get_full_name_to_string()
