686c1ff0ff356da43a2f55e185244a9aeda724e4
Jeff Folster <jmf432@mail.usask.ca>
2020-11-10 12:05:50 -0600

    Added more tests for Names.

--- tests/test_names.py
+++ tests/test_names.py
@@ -2,18 +2,39 @@ import pytest
 from Names import *
 
 
-def test_middle_names_set_names():
-    mn = MiddleNames()
-    mn.set_name(["Gary", "Kaye"])
-    assert ["Gary", "Kaye"] == mn.get_names_list()
-
-
+# GivenName tests
 def test_given_name_getter_setter():
     gn = GivenName()
     gn.set_name(["Kenny"])
     assert ["Kenny"] == gn.get_name()
 
 
+# MiddleNames tests
+def test_middle_names_set_names():
+    mn = MiddleNames()
+    mn.set_name(["Gary", "Kaye"])
+    assert ["Gary", "Kaye"] == mn.get_names_list()
+
+
+def test_middle_names_get_name():
+    mn = MiddleNames()
+    mn.set_name(["William", "Thomas"])
+    assert "William" == mn.get_name()
+
+
+def test_middle_names_get_names_list():
+    mn = MiddleNames()
+    mn.set_name(["William", "Thomas"])
+    assert ["William", "Thomas"] == mn.get_names_list()
+
+
+def test_middle_names_get_names_to_string():
+    mn = MiddleNames()
+    mn.set_name(["William", "Thomas"])
+    assert "William Thomas" == mn.get_names_to_string()
+
+
+# FullName tests
 def test_full_name_getters_setters():
     fn = FullName()
     fn.set_given_name("David")
