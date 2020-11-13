703ce32cdeefb651a065dae11386263c41d605b6
Oluwaseun Fashoranti <oof554@usask.ca>
2020-11-13 11:08:30 -0400

    Started working on medication testing commit function

---
+++ src/Medication.py
@@ -0,0 +1,10 @@
+class Medication:
+    def __init__(self):
+        self.scientific_name = ''
+        self.market_names = []
+
+    def get_scientific_name(self) -> str:
+        """
+
+        """
+        return self.scientific_name
