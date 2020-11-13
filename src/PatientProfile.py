725cb0f4c5c67a27297445607631a05cea898bdd
Jeff Folster <jmf432@mail.usask.ca>
2020-11-13 13:10:31 -0600

    Added basic implementation of PatientProfile.

---
+++ src/PatientProfile.py
@@ -0,0 +1,49 @@
+from Names import *
+from Note import *
+from Labwork import *
+from Allergies import *
+
+
+# TODO: import the classes that haven't been pushed to master yet.
+# from Medications import *
+# from Demographics import *
+
+class PatientProfile:
+    def __init__(self, full_name, demographics, notes, billing_code, medications, allergies, lab_work):
+        """
+        Constructor for PatientProfile.
+        :param full_name: A FullName object.
+        :param demographics: A list of Demographic objects.
+        :param notes: A list of Note objects.
+        :param billing_code: An integer.
+        :param medications: A list of Medication objects.
+        :param allergies: A list of Allergy objects
+        :param lab_work: A list of LabWork objects.
+        """
+        self.profile = {
+            'name': full_name,
+            'demographics': demographics,
+            'notes': notes,
+            'billing_code': billing_code,
+            'drugs': medications,
+            'allergies': allergies,
+            'lab_work': lab_work
+        }
+
+    def get_name(self):
+        return self.profile['name']
+
+    def get_demographics(self):
+        return self.profile['demographics']
+
+    def get_billing(self):
+        return self.profile['billing_code']
+
+    def get_notes(self):
+        return self.profile['notes']
+
+    def get_drugs(self):
+        return self.profile['drugs']
+
+    def get_lab_work(self):
+        return self.profile['lab_work']
