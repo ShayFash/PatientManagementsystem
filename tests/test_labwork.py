5c5416b943fc44470b00b368e23e5804854cf1ca
Jeff Folster <jmf432@mail.usask.ca>
2020-11-20 12:10:48 -0600

    added a LabWork test for make_request()

--- tests/test_labwork.py
+++ tests/test_labwork.py
@@ -2,6 +2,15 @@ import pytest
 
 from Labwork import *
 
+# dictionary input for a lab request in LabTest.
+request_input = {
+    'sender': 'Dr. Pipper',
+    'recipient': 'Dr. Robert',
+    'patient_ID': '6060842',
+    'details': 'We applied the cortical electrodes but '
+               'were unable to get a neural response.'
+}
+
 
 # LabResult tests
 def test_lab_get_empty_result():
@@ -20,3 +29,15 @@ def test_lab_set_result():
     lr = LabResult()
     lr.set_result("Results are inconclusive")
     assert "Results are inconclusive" == lr.get_result()
+
+
+# LabTest tests
+def test_lab_test_make_request():
+    lt = LabTest()
+    expected = "sent by: Dr. Pipper, sent to: Dr. Robert, " \
+               "patient ID: 6060842, details: We applied the cortical " \
+               "electrodes but were unable to get a neural response."
+    lt.make_request(request_input)
+    assert expected == lt.get_request()
+
+
