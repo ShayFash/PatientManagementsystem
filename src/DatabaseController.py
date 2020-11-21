64e987849ffc1c8725d3788846693dd5ba0fe1e4
Sam <samuelguttormson@gmail.com>
2020-11-20 21:26:08 -0600

    Addition of a barebones database controller class in src

---
+++ src/DatabaseController.py
@@ -0,0 +1,19 @@
+import PatientProfile
+
+class DatabaseController(object):
+    #going to focus on getting these few working and making sense first
+    def get_patient(health_num):
+        pass
+
+    def overwrite_patient(health_num, patient: PatientProfile):
+        pass
+
+    def insert_note(health_num, json_note):
+        pass
+
+    def set_billing(health_num, billing_code):
+        pass
+
+
+
+
