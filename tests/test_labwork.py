bfce7cf82b7b2a7013f3e73dfc5e3c04f3ea5e05
Jeff Folster <jmf432@mail.usask.ca>
2020-11-21 12:52:56 -0600

    added tests in Labwork test file.

--- tests/test_labwork.py
+++ tests/test_labwork.py
@@ -31,13 +31,37 @@ def test_lab_set_result():
     assert "Results are inconclusive" == lr.get_result()
 
 
+# LabRequest Tests
+def test_lab_request_set_request():
+    lr = LabRequest(request_input['sender'],
+                    request_input['recipient'],
+                    request_input['patient_ID'],
+                    request_input['details'])
+    # setting request changes the details field.
+    lr.set_request("What has science done?")
+    expected = "sent by: Dr. Pipper, sent to: Dr. Robert, " \
+               "patient ID: 6060842, details: What has science done?"
+    assert expected == lr.get_request()
+
+
 # LabTest tests
 def test_lab_test_make_request():
     lt = LabTest()
+    lt.make_request(request_input)
     expected = "sent by: Dr. Pipper, sent to: Dr. Robert, " \
                "patient ID: 6060842, details: We applied the cortical " \
                "electrodes but were unable to get a neural response."
-    lt.make_request(request_input)
     assert expected == lt.get_request()
+
+
+def test_lab_test_enter_results():
+    lt = LabTest()
+    lt.make_request(request_input)
+    lt.enter_results("Survey says: not great", [])
+    expected_string = "Survey says: not great"
+    expected_list = []
+    (result_string, result_list) = lt.get_results()
+    assert expected_string == result_string
+    assert expected_list == result_list
 
 
