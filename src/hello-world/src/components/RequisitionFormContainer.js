db697f881cf5248803a96e4fedcda92d362d0d5d
AdamBaker01134 <72470135+AdamBaker01134@users.noreply.github.com>
2020-11-27 20:15:56 -0600

    Added requisition form mock-ups

--- src/hello-world/src/components/RequisitionFormContainer.js
+++ src/hello-world/src/components/RequisitionFormContainer.js
@@ -1,34 +1,67 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PatientProfileContainer from './PatientProfileContainer';
 
 function BloodWorkForm() {
+    const [requested, setRequested] = useState(false);
+
+    function changeRequested(event) {
+        setRequested(true);
+    }
+
+    const bloodWorkTypes = ["FSH/LH", "STI", "H1AC", "etc"];
+    const bloodWorkElements = []
+    for (var i = 0; i < bloodWorkTypes.length; i++) {
+        bloodWorkElements.push(
+            <div>
+                <input type="checkbox"></input>
+                <span style={{fontSize: "24px"}}>{bloodWorkTypes[i]}</span>
+                <br /><br />
+            </div>
+        )
+    }
     return (
         <div className="homeContainer">
             <PatientProfileContainer/>
             <div style={{textAlign: "left", marginLeft: "5%"}}>
-                <h1>BloodWork</h1>
+                <h1>Blood Work{requested && " -- request submitted"}</h1>
+                {bloodWorkElements}
+                <button className="navBarButton" onClick={changeRequested} >Request</button>
             </div>
         </div>
     )
 }
 
 function ImagingForm() {
+    const [requested, setRequested] = useState(false);
+
+    function changeRequested(event) {
+        setRequested(true);
+    }
+
     return (
         <div className="homeContainer">
             <PatientProfileContainer/>
-            <form style={{textAlign: "left", marginLeft: "5%"}}>
-                <h1>Imaging</h1>
-            </form>
+            <div style={{textAlign: "left", marginLeft: "5%"}}>
+                <h1>Imaging{requested && " -- request submitted"}</h1>
+                <button className="navBarButton" onClick={changeRequested} >Request</button>
+            </div>
         </div>
     )
 }
 
 function ECGForm() {
+    const [requested, setRequested] = useState(false);
+
+    function changeRequested(event) {
+        setRequested(true);
+    }
+
     return (
         <div className="homeContainer">
             <PatientProfileContainer/>
             <div style={{textAlign: "left", marginLeft: "5%"}}>
-                <h1>ECG</h1>
+                <h1>ECG{requested && " -- request submitted"}</h1>
+                <button className="navBarButton" onClick={changeRequested} >Request</button>
             </div>
         </div>
     )
