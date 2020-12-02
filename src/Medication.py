226ade3acb0eac27b425d9de12dc6b019fccc66c
Oluwaseun Fashoranti <oof554@usask.ca>
2020-12-01 20:14:58 -0400

    Redid the Def _init_, cleaned up a few errors

--- src/Medication.py
+++ src/Medication.py
@@ -1,7 +1,10 @@
 class Medication:
-    def __init__(self):
-        self.scientific_name = ''
-        self.market_names = []
+    def __init__(self, unique_id, medicine_name, chemical_name, synonym, suppress):
+        self.id = unique_id
+        self.medicine_name = medicine_name
+        self.chemical_name = chemical_name
+        self.synonym = synonym
+        self.suppress = suppress
 
     def get_scientific_name(self):
         """
@@ -12,11 +15,11 @@ class Medication:
 
     @property
     def get_market_names(self):
-       """
+        """
        Return all the market names of drug
        :return: list of drug market names
        """
-       return self.market_names
+        return self.market_names
 
     def set_scientific_name(self, name):
         """
@@ -27,9 +30,15 @@ class Medication:
 
     def set_market_names(self, market_names):
         """
-        :param  market_name: Market names of medicine
+        :param  market_names: Market names of medicine
         :return: None
         """
         self.market_names = market_names
 
-
+    def __str__(self):
+        return f"""
+        Medicine Name: {self.medicine_name}
+        Chemical Name: {self.chemical_name}
+        Synonym: {self.synonym}
+        Suppress: {self.suppress}
+        """
